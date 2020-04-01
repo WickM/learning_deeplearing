06ff683076130e35bbbcdf2fb8da344b0247df8f
WickM <manuel.wick@gmail.com>
2020-04-02 00:56:33 +0200

    week2 Nlp

--- NaturalLanguageProcessing/week1.py
+++ NaturalLanguageProcessing/week1.py
@@ -1,4 +1,31 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
 
+import tensorflow as tf
+import numpy as np 
+
+print(tf.__version__)
+
+import tensorflow_datasets as tfds
+
+imdb, info = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+
+tf.
+  
+train_data, test_data = imdb['train'], imdb['test']
+
+training_sentences = []
+training_labels = []
+
+testing_sentences = []
+testing_labels = []
+
+# str(s.tonumpy()) is needed in Python3 instead of just s.numpy()
+for s,l in train_data:
+  training_sentences.append(str(s.numpy()))
+  training_labels.append(l.numpy())
+  
+for s,l in test_data:
+  testing_sentences.append(str(s.numpy()))
+  testing_labels.append(l.numpy())
+  
+training_labels_final = np.array(training_labels)
+testing_labels_final = np.array(testing_labels)
---
+++ config Python.R
@@ -0,0 +1,5 @@
+#Run befor any Python skript
+
+library(reticulate)
+reticulate::use_condaenv(condaenv = "deeplearning")
+
